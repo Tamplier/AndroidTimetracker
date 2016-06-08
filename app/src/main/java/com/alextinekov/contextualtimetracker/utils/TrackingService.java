df0457d1d359ea5f7ed31765e4436abb33817b72
Tamplier <araman777@gmail.com>
2016-06-08 13:44:51 +0700

    Notification update, is screen on checking and searching foreground process in list

--- app/src/main/java/com/alextinekov/contextualtimetracker/utils/TrackingService.java
+++ app/src/main/java/com/alextinekov/contextualtimetracker/utils/TrackingService.java
@@ -2,6 +2,7 @@ package com.alextinekov.contextualtimetracker.utils;
 
 import android.app.ActivityManager;
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -14,6 +15,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.PowerManager;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -38,14 +40,29 @@ public class TrackingService extends Service {
     public static final int MSG_SAVE_STATE = 1;
     private static final String SYSTEM_PACKAGE_NAME = "android";
     private RunnedApplicationInfo applicationsArray[];
-    private long prevTime;
+    private long prevTime = 0;
+    private boolean prevScreenState = true;
     @Override
     public void onCreate() {
         super.onCreate();
         Log.i(TAG, "Service created");
 
+        updateNotification("none");
+
+        applicationsArray = new RunnedApplicationInfo[10];
+    }
+
+    private void updateNotification(String foregroundApp){
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent intent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
         Notification.Builder builder = new Notification.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher);
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getResources().getString(R.string.app_name))
+                .setContentText(String.format(getResources().getString(R.string.foreground_app), foregroundApp))
+                .setContentIntent(intent);
         Notification notification;
         if (Build.VERSION.SDK_INT < 16)
             notification = builder.getNotification();
@@ -53,8 +70,6 @@ public class TrackingService extends Service {
             notification = builder.build();
 
         startForeground(SERVICE_ID, notification); //не убивать сервис при нехватке памяти и при удалении приложния из недавних.
-
-        applicationsArray = new RunnedApplicationInfo[10];
     }
 
     @Override
@@ -106,16 +121,35 @@ public class TrackingService extends Service {
     }
 
     private void updateAppsList(){
+        boolean isScreenOn = isScreenOn();
+        if(prevScreenState != isScreenOn){
+            prevTime = 0;
+        }
+        if(!isScreenOn){
+            Log.i(TAG, "Screen off, no interaction");
+            return;
+        }
         ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        /**
+         * Returns a list of RunningAppProcessInfo records, or null if there are no running processes
+         * (it will not return an empty list).
+         * This list ordering is not specified.
+         */
         List<ActivityManager.RunningAppProcessInfo> processes = activityManager.getRunningAppProcesses();
-        ActivityManager.RunningAppProcessInfo info = processes.get(0);
+        ActivityManager.RunningAppProcessInfo info = getForegroundProcess(processes);
+        if(prevTime == 0) prevTime = System.currentTimeMillis();
+
+        long deltaTime = System.currentTimeMillis() - prevTime;
+        prevTime = System.currentTimeMillis();
+
+        if(info == null || BuildConfig.APPLICATION_ID.equals(info.processName)){
+            Log.i(TAG, "There is no foreground processes or not tracked package is found");
+            updateNotification("none");
+            return;
+        }
         PackageManager pm = this.getPackageManager();
         CharSequence label;
         try{
-            if(!isForegroundProcess(info) || BuildConfig.APPLICATION_ID.equals(info.processName)) {
-                Log.i(TAG, "Not tracked package is found: " + info.processName);
-                return;
-            }
             ApplicationInfo appInfo = pm.getApplicationInfo(info.processName, PackageManager.GET_META_DATA);
             label = pm.getApplicationLabel(appInfo);
         }
@@ -123,8 +157,10 @@ public class TrackingService extends Service {
             label = info.processName;
         }
 
+        updateNotification(label.toString());
+
         if(applicationsArray[0] != null){
-            applicationsArray[0].activeTime += TRACK_PERIOD_IN_MILLIS;
+            applicationsArray[0].activeTime += deltaTime;
         }
         if(applicationsArray[0] == null || !info.processName.equals(applicationsArray[0].packageName)){
             RunnedApplicationInfo rai = new RunnedApplicationInfo();
@@ -138,8 +174,31 @@ public class TrackingService extends Service {
         Log.i(TAG, String.format("Top process %s. Time: %d", label, applicationsArray[0].activeTime));
     }
 
-    private boolean isForegroundProcess(ActivityManager.RunningAppProcessInfo processInfo){
-        return processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND;
+    private ActivityManager.RunningAppProcessInfo getForegroundProcess(List<ActivityManager.RunningAppProcessInfo> processes){
+        ActivityManager.RunningAppProcessInfo foreground = null;
+        if(processes == null)
+            return foreground;
+        for(ActivityManager.RunningAppProcessInfo pInfo : processes){
+            /*
+            * This process is running the foreground UI;
+            * that is, it is the thing currently at the top of the screen that the user is interacting with.
+            * */
+            if(pInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
+                foreground = pInfo;
+                break;
+            }
+        }
+        return foreground;
+    }
+
+    private boolean isScreenOn(){
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+        boolean result = false;
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.KITKAT_WATCH)
+            result = pm.isInteractive();
+        else
+            result = pm.isScreenOn();
+        return result;
     }
 
     private Handler interactionHandler = new Handler(){
